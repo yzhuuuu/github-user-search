6b0e9541462f73cac81539d8442664764e4ffb46
yuhang zhu <zhucolley@outlook.com>
2024-02-06 19:50:03 -0500

    init project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className={"font-lato"}>App</div>
+      <button className="btn btn-primary btn-sm">Primary</button>
+    </>
+  );
+}
+
+export default App;
